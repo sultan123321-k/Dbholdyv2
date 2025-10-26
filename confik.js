94d7ae235987e853f5d3998e4def628d7deeaced
sultan123321-k <sultanrezky5@gmail.com>
2025-10-26 18:32:16 +0700

    Update token list

--- confik.js
+++ confik.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "8270546983:AAFI5e9xESPdJphXyL9vppy4MiyoHBEWcCI"
+    "8270546983:AAFI5e9xESPdJphXyL9vppy4MiyoHBEWcCI",
+    "8324349698:AAEzqpK1mvV4fWE_R9h2frXLMGsSGA49kSQ"
   ]
 }
